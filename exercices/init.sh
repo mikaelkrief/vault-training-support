2e18344c31a4dbe864f05a66f6060d432f9ae684
Mikael <krief_mikael@hotmail.com>
2023-11-28 19:31:30 +0100

    Update init.sh

--- exercices/init.sh
+++ exercices/init.sh
@@ -1,11 +1,1 @@
-mkdir -p /opt/vault/data
-cd /opt/vault
-tee config.hcl <<EOF
-ui = true
-disable_mlock = true
-
-api_addr = "http://127.0.0.1:8200"
-cluster_addr = "https://127.0.0.1:8201"
-EOF
-
-vault server -dev -config=/opt/vault/data/config.hcl
+vault server -config /etc/vault.d/vault.hcl -dev
